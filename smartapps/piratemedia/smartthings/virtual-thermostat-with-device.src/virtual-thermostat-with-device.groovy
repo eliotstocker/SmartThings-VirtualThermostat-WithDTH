1f3bae2ae366572ce0e812ec749c751347f4dc76
Eliot Stocker <eliot@piratemedia.tv>
2020-04-05 18:46:40 +0100

    tidied the app too

--- smartapps/piratemedia/smartthings/virtual-thermostat-with-device.src/virtual-thermostat-with-device.groovy
+++ smartapps/piratemedia/smartthings/virtual-thermostat-with-device.src/virtual-thermostat-with-device.groovy
@@ -16,14 +16,14 @@ preferences {
 	section("Select the heater outlet(s)... "){
 		input "outlets", "capability.switch", title: "Outlets", multiple: true
 	}
-	section("Only heat when contact isnt open (optional, leave blank to not require contact sensor)..."){
-		input "motion", "capability.contactSensor", title: "Contact", required: false
+	section("Only heat when contact(s) arent open (optional, leave blank to not require contact sensor)..."){
+		input "motion", "capability.contactSensor", title: "Contact", required: false, multiple: true
 	}
 	section("Never go below this temperature: (optional)"){
 		input "emergencySetpoint", "decimal", title: "Emergency Temp", required: false
 	}
 	section("Temperature Threshold (Don't allow heating to go above or bellow this amount from set temperature)") {
-		input "threshold", "decimal", "title": "Temperature Threshold", required: false, defaultValue: 1.0
+		input "threshold", "decimal", title: "Temperature Threshold", required: false, defaultValue: 1.0
 	}
 }
 
@@ -31,28 +31,13 @@ def installed()
 {
     log.debug "running installed"
     state.deviceID = Math.abs(new Random().nextInt() % 9999) + 1
-	state.lastTemp = null
-    state.contact = true
-    state.todayTime = 0
-    state.yesterdayTime = 0
-    state.date = new Date().format("dd-MM-yy")
-    state.lastOn = 0
-    /*def thermostat = createDevice()
-    
-	subscribe(sensor, "temperature", temperatureHandler)
-	if (motion) {
-		subscribe(motion, "motion", motionHandler)
-	}
-    
-    subscribe(thermostat, "thermostatSetpoint", thermostatTemperatureHandler)
-    subscribe(thermostat, "thermostatMode", thermostatModeHandler)
-    thermostat.setVirtualTemperature(sensor.currentValue("temperature"))*/
 }
 
 def createDevice() {
     def thermostat
     def label = app.getLabel()
-	// Commenting out hub refernce - breaks in several thermostat DHs
+    
+
     log.debug "create device with id: pmvt$state.deviceID, named: $label" //, hub: $sensor.hub.id"
     try {
         thermostat = addChildDevice("piratemedia/smartthings", "Virtual Thermostat Device", "pmvt" + state.deviceID, null, [label: label, name: label, completedSetup: true])
@@ -60,6 +45,92 @@ def createDevice() {
         log.error("caught exception", e)
     }
     return thermostat
+}
+
+def shouldHeatingBeOn(thermostat) {    
+    //if temperature is bellow emergency setpoint
+    if(emergencySetpoint > getAverageTemperature()) {
+    	return true;
+    }
+    
+	//if thermostat isnt set to heat
+	if(thermostat.currentValue('thermostatMode') != "heat") {
+    	return false;
+    }
+    
+    //if any of the contact sensors are open
+    if(motion) {
+    	for(m in motion) {
+			if(m.currentValue('contact') == "open") {
+            	return false;
+            }
+        }
+    }
+    
+    //average temperature across all temperateure sensors is above set point
+    if (thermostat.currentValue("thermostatSetpoint") - getAverageTemperature() >= threshold) {
+    	return false;
+    }
+    
+    return true;
+}
+
+def getHeatingStatus(thermostat) {    
+    //if temperature is bellow emergency setpoint
+    if(emergencySetpoint > getAverageTemperature()) {
+    	return 'heating';
+    }
+    
+	//if thermostat isnt set to heat
+	if(thermostat.currentValue('thermostatMode') != "heat") {
+    	return 'idle';
+    }
+    
+    //if any of the contact sensors are open
+    if(motion) {
+    	for(m in motion) {
+			if(m.currentValue('contact') == "open") {
+            	return 'pending heat';
+            }
+        }
+    }
+    
+    //average temperature across all temperateure sensors is above set point
+    if (thermostat.currentValue("thermostatSetpoint") - getAverageTemperature() >= threshold) {
+    	return 'idle';
+    }
+    
+    return 'heat';
+}
+
+def getAverageTemperature() {
+	def total = 0;
+    def count = 0;
+    
+    //total all sensors temperature
+	for(sensor in sensors) {
+    	total += sensor.currentValue("temperature")
+        thermostat.setIndividualTemperature(sensor.currentValue("temperature"), count, sensor.label)
+        count++
+    }
+    
+    //divide by number of sensors
+    return total / count
+}
+
+def handleChange() {
+	def thermostat = getThermostat()
+
+	//update device
+    thermostat.setHeatingStatus(getHeatingStatus(thermostat))
+    thermostat.setVirtualTemperature(getAverageTemperature())
+    
+    //set heater outlet
+    if(shouldHeatingBeOn(thermostat)) {
+    	outlets.on()
+    } else {
+    	outlets.off()
+    }
 }
 
 def getThermostat() {
@@ -78,186 +149,49 @@ def updated()
     log.debug "running updated: $app.label"
 	unsubscribe()
     unschedule()
+    
+    //get or add thermostat
     def thermostat = getThermostat()
     if(thermostat == null) {
         thermostat = createDevice()
     }
-    state.contact = true
-	state.lastTemp = null
-    if(state.todayTime == null) state.todayTime = 0
-    if(state.yesterdayTime == null) state.yesterdayTime = 0
-    if(state.date == null) state.date = new Date().format("dd-MM-yy")
-    if(state.lastOn == null) state.lastOn = 0
+    
+    //subscribe to temperatuire changes
 	subscribe(sensors, "temperature", temperatureHandler)
+    
+    //subscribe to contact sensor changes
 	if (motion) {
 		subscribe(motion, "contact", motionHandler)
 	}
+    
+    //subscribe to virtual device changes
     subscribe(thermostat, "thermostatSetpoint", thermostatTemperatureHandler)
     subscribe(thermostat, "thermostatMode", thermostatModeHandler)
+    
+    //reset some values
     thermostat.clearSensorData()
     thermostat.setVirtualTemperature(getAverageTemperature())
 	thermostat.setTemperatureScale(parent.getTempScale())
-    runEvery1Hour(updateTimings)
-}
-
-def getAverageTemperature() {
-	def total = 0;
-    def count = 0;
-	for(sensor in sensors) {
-    	total += sensor.currentValue("temperature")
-        thermostat.setIndividualTemperature(sensor.currentValue("temperature"), count, sensor.label)
-        count++
-    }
-    return total / count
 }
 
 def temperatureHandler(evt)
 {
-    def thermostat = getThermostat()
-    thermostat.setVirtualTemperature(getAverageTemperature())
-	if (state.contact || emergencySetpoint) {
-		evaluate(evt.doubleValue, thermostat.currentValue("thermostatSetpoint"))
-        state.lastTemp = evt.doubleValue
-	}
-	else {
-		heatingOff()
-	}
+    handleChange()
 }
 
 def motionHandler(evt)
 {
-    def thermostat = getThermostat()
-	if (evt.value == "closed") {
-    	state.contact = true
-		def thisTemp = getAverageTemperature()
-		if (thisTemp != null) {
-			evaluate(thisTemp, thermostat.currentValue("thermostatSetpoint"))
-			state.lastTemp = thisTemp
-		}
-	} else if (evt.value == "open") {
-        log.debug "should turn heating off"
-    	state.contact = false
-	    heatingOff()
-	}
+    handleChange()
 }
 
 def thermostatTemperatureHandler(evt) {
-	def temperature = evt.doubleValue
-    //setpoint = temperature
-	log.debug "Desired Temperature set to: $temperature $state.contact"
-    
-    def thisTemp = getAverageTemperature()
-	if (state.contact) {
-		evaluate(thisTemp, temperature)
-	}
-	else {
-		heatingOff()
-	}
+	handleChange()
 }
 
 def thermostatModeHandler(evt) {
-	def mode = evt.value
-	log.debug "Mode Changed to: $mode"
-    def thermostat = getThermostat()
-    
-    def thisTemp = getAverageTemperature()
-	if (state.contact) {
-		evaluate(thisTemp, thermostat.currentValue("thermostatSetpoint"))
-	}
-	else {
-		heatingOff(mode == 'heat' ? false : true)
-	}
-}
-
-private evaluate(currentTemp, desiredTemp)
-{
-	log.debug "EVALUATE($currentTemp, $desiredTemp)"
-	// heater
-	if ( (desiredTemp - currentTemp >= threshold)) {
-		heatingOn()
-	} else if ( (currentTemp - desiredTemp >= threshold)) {
-		heatingOff()
-	} else if(state.current == "on") {
-        updateTimings()
-    }
-}
-
-def heatingOn() {
-    if(thermostat.currentValue('thermostatMode') == 'heat' || force) {
-    	log.debug "Heating on Now"
-        outletsOn()
-        thermostat.setHeatingStatus(true)
-    } else {
-        heatingOff(true)
-    }
-}
-
-def heatingOff(heatingOff) {
-	def thisTemp = getAverageTemperature()
-    if (thisTemp <= emergencySetpoint) {
-        log.debug "Heating in Emergency Mode Now"
-        ouletsOn()
-        thermostat.setEmergencyMode(true)
-    } else {
-    	log.debug "Heating off Now"
-    	outletsOff()
-		if(heatingOff) {
-			thermostat.setHeatingOff(true)
-		} else {
-			thermostat.setHeatingStatus(false)
-		}
-    }
+	handleChange()
 }
 
 def updateTempScale() {
 	thermostat.setTemperatureScale(parent.getTempScale())
-}
-
-def updateTimings() {
-    def date = new Date().format("dd-MM-yy")
-    if(state.current == "on") {
-        int time = Math.round(new Date().getTime() / 1000) - state.lastOn
-        state.todayTime = state.todayTime + time
-        state.lastOn = Math.round(new Date().getTime() / 1000)
-    }
-    if(state.date != date) {
-        state.yesterdayTime = state.todayTime
-        state.date = date
-        state.todayTime = 0
-    }
-    thermostat.setTimings(state.todayTime, state.yesterdayTime)
-}
-
-def outletsOn() {
-    outlets.on()
-    def date = new Date().format("dd-MM-yy")
-    if(state.current == "on") {
-        int time = Math.round(new Date().getTime() / 1000) - state.lastOn
-        state.todayTime = state.todayTime + time
-    }
-    if(state.date != date) {
-        state.yesterdayTime = state.todayTime
-        state.date = date
-        state.todayTime = 0
-    }
-    state.lastOn = Math.round(new Date().getTime() / 1000)
-    state.current = "on"
-    thermostat.setTimings(state.todayTime, state.yesterdayTime)
-}
-
-def outletsOff() {
-    outlets.off()
-    def date = new Date().format("dd-MM-yy")
-    if(state.current == "on") {
-        int time = Math.round(new Date().getTime() / 1000) - state.lastOn
-        state.todayTime = state.todayTime + time
-    }
-    if(state.date != date) {
-        state.yesterdayTime = state.todayTime
-        state.date = date
-        state.todayTime = 0
-    }
-    state.current = "off"
-    state.lastOn = 0;
-    thermostat.setTimings(state.todayTime, state.yesterdayTime)
 }
